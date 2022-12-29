9bcc55063f9dc295a4b6ae16d623067615b74e12
Bladesheng <keadr23@gmail.com>
2022-12-29 13:45:01 +0100

    Set up dependencies

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req: Request, res: Response) => {
+  res.sendStatus(404);
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`[server] Server is running at http://localhost:${port}`);
+});
