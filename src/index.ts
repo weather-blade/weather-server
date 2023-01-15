3dda047c584523a8660747c37d2c57bfeda95b28
Bladesheng <keadr23@gmail.com>
2023-01-15 16:30:22 +0100

    Allow CORS requests from weather-station-frontend website

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 
@@ -12,6 +12,23 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// CORS handling
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const allowedOrigins = [
+    "https://bladesheng.github.io/weather-station-frontend/",
+    "http://localhost:8000",
+  ];
+
+  const origin = String(req.headers.origin);
+  const resOrigin = allowedOrigins.indexOf(origin) >= 0 ? origin : allowedOrigins[0];
+
+  res.header("Access-Control-Allow-Origin", resOrigin);
+  res.header("Access-Control-Allow-Headers", "Content-Type, password, short");
+  res.header("Access-Control-Max-Age", "86400"); // skip recurent preflight requests for 24h
+
+  next();
+});
 
 app.use("/api", apiRouter);
 
