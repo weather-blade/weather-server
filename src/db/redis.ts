579348bbcb55eba62417af444b19286116d17ac8
Bladesheng <keadr23@gmail.com>
2023-08-26 16:15:43 +0200

    Redis caching for the last 24 hours readings endpoint

---
+++ src/db/redis.ts
@@ -0,0 +1,9 @@
+import redis from "redis";
+
+export const redisClient = redis.createClient();
+
+redisClient.on("error", (err) => {
+  console.error("Redis Client Error", err);
+});
+
+await redisClient.connect();
